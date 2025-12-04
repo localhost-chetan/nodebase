60ca15e1645a42c572d9d52accbcd3320f1d437b
Chetan Seervi <chetanseervi513@gmail.com>
2025-12-04 16:45:34 +0530

    Add .gitignore file

--- backend/migrate.ts
+++
@@ -1,7 +0,0 @@
-import { getDatabaseUrl } from "@utils/db";
-import { drizzle } from "drizzle-orm/node-postgres"
-import { migrate } from 'drizzle-orm/node-postgres/migrator';
-
-const db = drizzle(getDatabaseUrl());
-
-await migrate(db, { migrationsFolder: "./drizzle/migrations" });
