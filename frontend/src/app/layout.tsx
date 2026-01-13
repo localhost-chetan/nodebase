f1e12657e4d114b0bb44d863bd4e6c26299cf01f
Chetan Seervi <chetanseervi513@gmail.com>
2026-01-13 15:03:10 +0530

    add biome formatter

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,47 +3,47 @@ import { type Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: { template: "%s | Nodebase", default: "Nodebase" },
-	description:
-		"Nodebase is an open-source n8n clone for workflow automation. Build, automate, and integrate tasks visually with powerful nodes, seamless integrations, and a user-friendly interface.",
+  title: { template: "%s | Nodebase", default: "Nodebase" },
+  description:
+    "Nodebase is an open-source n8n clone for workflow automation. Build, automate, and integrate tasks visually with powerful nodes, seamless integrations, and a user-friendly interface.",
 
-	creator: "Chetan Seervi",
-	keywords: [
-		"n8n",
-		"n8n clone",
-		"workflow automation",
-		"open-source",
-		"automation platform",
-		"integration tools",
-		"visual workflow builder",
-		"task automation",
-		"workflow management",
-		"automation software",
-	],
+  creator: "Chetan Seervi",
+  keywords: [
+    "n8n",
+    "n8n clone",
+    "workflow automation",
+    "open-source",
+    "automation platform",
+    "integration tools",
+    "visual workflow builder",
+    "task automation",
+    "workflow management",
+    "automation software",
+  ],
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" className="dark">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,20 +2,33 @@ import { Button } from "@shadcn/button";
 import { client } from "@lib/rpc-client";
 
 export default async function Page() {
-
-	const response = await client.index.$get()
+	const response = await client.index.$get();
 	if (response.ok) {
-		const data = await response.json();
+		const data = await response.json()
+
 		console.log("🚀 ------------------------------------------------------🚀");
 		console.log("🚀 ~ page.tsx:6 ~ data: ", data);
 		console.log("🚀 ------------------------------------------------------🚀");
-		return (<div>{data}
-			<Button>Click Here</Button>
-		</div>);
+
+		console.log("Hello world")
+
+
+		return (
+			<div>
+				{data}
+				<Button>Click Here</Button>
+
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem tempore,
+					hic rem inventore ex odio at, eligendi maxime dignissimos
+					assumenda reprehenderit, cupiditate magni nesciunt debitis nobis
+					consectetur veniam. Beatae, similique.
+				</p>
+
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quaerat rem dolore assumenda deserunt ipsum provident. Provident sapiente optio repudiandae cumque dolore omnis nobis obcaecati porro quos doloribus, perferendis incidunt!</p>
+			</div>
+		);
 	}
 
-	return <div>Failed to load data
-
-	</div>;
-
+	return <div>Failed to load data</div>;
 }
